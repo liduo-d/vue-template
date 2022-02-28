dfb0b6578099de80f874dbb91dea2c8c754de94d
liduo-d <leedfxbliduo@163.com>
2022-02-28 23:01:13 +0800

    Add Markdown, Richtext

--- src/views/tools/markdown/index.vue
+++ src/views/tools/markdown/index.vue
@@ -1,10 +1,25 @@
 <script setup>
+    import {mavonEditor} from 'mavon-editor';
+    import 'mavon-editor/dist/css/index.css';
 
+    import {ref} from 'vue';
+
+    const value = ref('');
+    const submit = () => {
+        // submit value.value
+    };
+</script>
+
+<script>
+    export default {
+        name: 'Markdown'
+    }
 </script>
 
 <template>
     <div>
-        markdown
+        <mavonEditor ref="markdownEditor" v-model="value" :language="'en'"></mavonEditor>
+        <el-button style="margin-top: 30px;" @click="submit">Submit</el-button>
     </div>
 </template>
 
--- src/views/tools/richtext/index.vue
+++ src/views/tools/richtext/index.vue
@@ -1,13 +1,44 @@
 <script setup>
+    import {onMounted, onBeforeUnmount} from "vue";
+    import WangEditor from "wangEditor";
+    import i18next from 'i18next';
+    let _richtext;
+    onMounted(() => {
+        _richtext = new WangEditor(document.getElementById('richtextEditor'));
+        _richtext.config.zIndex = 1;
+        // needs plugin i18next
+        _richtext.i18next = i18next;
+        _richtext.config.lang = 'en';
+        _richtext.config.placeholder = '';
+        _richtext.create();
+    });
+    onBeforeUnmount(() => {
+        _richtext.destroy();
+        _richtext = null;
+    });
+    const submit = () => {
+        const html = _richtext.txt.html();
+        // submit
+    };
+</script>
 
+<script>
+    export default {
+        name: 'RichText'
+    }
 </script>
 
 <template>
     <div>
-        richtext
+        <div id="richtextEditor" class="mt30">
+        </div>
+
+        <el-button class="mt30" @click="submit">Submit</el-button>
     </div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+    .mt30 {
+        margin-top: 30px;
+    }
 </style>
